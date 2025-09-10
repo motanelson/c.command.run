e0c58dd37bdde9d83925cb23f4d83c55ec290242
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-09-10 13:01:13 +0100

    Add files via upload

--- command.c
+++ command.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int main(void)
+int main(int argc , char *argv[])  
 {
                 /* chama command<numero> <estado>, numerando 1..16 */
                 char cmd[128];
+                printf("\033c\033[43;30m\n");
                 //change this line
-                snprintf(cmd, sizeof(cmd), "curl www.google.com");
+                snprintf(cmd, sizeof(cmd), "curl www.google.com/%s",argv[1]);
                 /* faz system call */
                 int rc = system(cmd);
                 
