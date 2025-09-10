69bc613a9ca0f9e9a6e6445c4f1e62b1e07e85fc
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-09-10 16:56:17 +0100

    Add files via upload

--- command.pas
+++ command.pas
@@ -1,6 +1,21 @@
-uses sysutils;
+program command;
+
+uses
+  Process;
+
+var
+  AProcess: TProcess;
 
 begin
-    writeln(chr(27)+'[43;30m]'+ParamStr(1));
-    executeprocess('/bin/curl',['www.google.com/', ParamStr(1)]);
+  writeln(#27'[43;30m', ParamStr(1));
+
+  AProcess := TProcess.Create(nil);
+  
+    AProcess.Executable := '/usr/bin/curl';
+    AProcess.Parameters.Add('https://www.google.com/');
+    AProcess.Parameters.Add(ParamStr(1));
+    AProcess.Options := AProcess.Options ;  // espera terminar
+    AProcess.Execute;
+  
+  
 end.
